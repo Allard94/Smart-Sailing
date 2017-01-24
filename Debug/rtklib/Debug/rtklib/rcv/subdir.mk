cb42f36b845879698f353c7e0bc8a7231d67e1ac
Allard <a.struiksma@hotmail.com>
2017-01-24 10:44:40 +0100

    final version

---
+++ Debug/rtklib/Debug/rtklib/rcv/subdir.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../rtklib/Debug/rtklib/rcv/binex.o \
+../rtklib/Debug/rtklib/rcv/crescent.o \
+../rtklib/Debug/rtklib/rcv/gw10.o \
+../rtklib/Debug/rtklib/rcv/javad.o \
+../rtklib/Debug/rtklib/rcv/novatel.o \
+../rtklib/Debug/rtklib/rcv/nvs.o \
+../rtklib/Debug/rtklib/rcv/rcvlex.o \
+../rtklib/Debug/rtklib/rcv/rt17.o \
+../rtklib/Debug/rtklib/rcv/septentrio.o \
+../rtklib/Debug/rtklib/rcv/skytraq.o \
+../rtklib/Debug/rtklib/rcv/ss2.o \
+../rtklib/Debug/rtklib/rcv/ublox.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
---
+++ Debug/rtklib/Debug/rtklib/subdir.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../rtklib/Debug/rtklib/convkml.o \
+../rtklib/Debug/rtklib/convrnx.o \
+../rtklib/Debug/rtklib/datum.o \
+../rtklib/Debug/rtklib/download.o \
+../rtklib/Debug/rtklib/ephemeris.o \
+../rtklib/Debug/rtklib/geoid.o \
+../rtklib/Debug/rtklib/ionex.o \
+../rtklib/Debug/rtklib/lambda.o \
+../rtklib/Debug/rtklib/options.o \
+../rtklib/Debug/rtklib/pntpos.o \
+../rtklib/Debug/rtklib/postpos.o \
+../rtklib/Debug/rtklib/ppp.o \
+../rtklib/Debug/rtklib/ppp_ar.o \
+../rtklib/Debug/rtklib/preceph.o \
+../rtklib/Debug/rtklib/qzslex.o \
+../rtklib/Debug/rtklib/rcvraw.o \
+../rtklib/Debug/rtklib/rinex.o \
+../rtklib/Debug/rtklib/rtcm.o \
+../rtklib/Debug/rtklib/rtcm2.o \
+../rtklib/Debug/rtklib/rtcm3.o \
+../rtklib/Debug/rtklib/rtcm3e.o \
+../rtklib/Debug/rtklib/rtkcmn.o \
+../rtklib/Debug/rtklib/rtkpos.o \
+../rtklib/Debug/rtklib/rtksvr.o \
+../rtklib/Debug/rtklib/sbas.o \
+../rtklib/Debug/rtklib/solution.o \
+../rtklib/Debug/rtklib/stream.o \
+../rtklib/Debug/rtklib/streamsvr.o \
+../rtklib/Debug/rtklib/tle.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
